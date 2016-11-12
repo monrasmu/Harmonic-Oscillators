1378b13fff098387c4ae75d8632b91ee4a99a599
monrasmu <monrasmu@umich.edu>
2016-11-12 08:19:16 -0500

    fixed putInPymol for n

--- HarmonicOscillator1D.py
+++ HarmonicOscillator1D.py
@@ -22,57 +22,66 @@ def generateAtoms(num):
 def calculateV(array):
 	# calculation of LJ potential using possible radii
 	# returns potential in kJ/mol
-	radius = cmath.sqrt((array[1][1] - array[2][1]) ** 2 
-					  + (array[1][2] - array[2][2]) ** 2 
-					  + (array[1][3] - array[2][3]) ** 2)
-	_V = ((4 * _e) * (((_d / radius) ** 12) - ((_d / radius) ** 6)))
-	return _V
+	for n, m in len(array):
+		radius_sqd = ((array[n][m] - array[n][m]) ** 2)
+	radius = cmath.sqrt(radius_sqd)
+	print radius
+	#_V = ((4 * _e) * (((_d / radius) ** 12) - ((_d / radius) ** 6)))
+	#return _V
 
 def putInPymol(array):
-	print array
 	# First cast array as strings
-	str_arr = [str(num) for num in array]
-	
+	def makeStr(myArray):
+		return map(str, myArray)
+	str_arr = map(makeStr, array)
+
+	# get dimensions of array and flatten to make life easier
+	x = np.array(str_arr)
+	flatArray = x.flatten()
+	size = flatArray.shape
+
 	# put into PyMol readable format
+	# must iterate every 3 as we've flattened the array
 	file = open('testfile.pdb', 'w')
-	file.write ('ATOM      1  H1   U 0  10      ' 
-				+ strx1 + '.000  ' + stry1 + '.000  ' 
-				+ strz1 + '.000 \n'
-			  + 'ATOM      2  H2   U 0  10      ' 
-				+ strx2 + '.000  ' + stry2 + '.000  ' 
-				+ strz2 + '.000 \n'
-			  + 'ATOM      3  H3   U 0  10      ' 
-				+ strx3 + '.000  ' + stry3 + '.000  ' 
-				+ strz3 + '.000 \n')
+	for n in range(0, size[0] - 2, 3):
+		file.write ('ATOM      1  H1   U 0  10      ' )
+		file.write(flatArray[n] + '.000  ' 
+				 + flatArray[n + 1] + '.000  '
+				 + flatArray[n + 2] + '.000  ')
+		file.write('\n')
 	file.close()
 
 
 
 class Test(unittest.TestCase):
-    # Test cases
-    data1 = [(2, (2, 3)),
-    		(3, (3, 3)),
-    		(0, (0, 3))]
-    data2 = 
-    data3 = [[2, 3],
-    		 [3, 4, 4]]
+	data1 = [(2, (2, 3)), (3, (3, 3)), (0, (0, 3))]
 
-    # test to see if generateAtoms generates
-    # the right size array
-    def test_generateAtoms(self):
-        for [test_num, expected_size] in self.data1:
-            (actual_array, actual_size) = generateAtoms(test_num)
-            self.assertEqual(actual_size, expected_size)
+	data3 = [([2, 3, 4],
+			 [1, 4, 1],
+			 [3, 2, 1]),
+			([2, 3, 3],
+			 [2, 5, 3],
+			 [4, 2, 2])]
 
-    # test to see if calculateV generates values
-    # COMPARE TO CALCULATED?
-    def test_calculateV(self):
-   		for array in self.data2:
+	# test to see if generateAtoms generates
+	# the right size array
+	def test_generateAtoms(self):
+		for [test_num, expected_size] in self.data1:
+		    (actual_array, actual_size) = generateAtoms(test_num)
+		    self.assertEqual(actual_size, expected_size)
 
-   	def test_putInPymol(self):
+	# test to see if calculateV generates values
+	# COMPARE TO CALCULATED?
+	"""
+	def test_calculateV(self):
+		for array in self.data2:
+	"""
+
+	# test putting arrays into Pymol coords
+	def test_putInPymol(self):
    		for array in self.data3:
    			putInPymol(array)
-
+	
 
 
 
