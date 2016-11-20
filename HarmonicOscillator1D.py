e74d0cdfabc74be7d2e0f17403c67ca19d8077ea
monrasmu <monrasmu@umich.edu>
2016-11-19 23:53:12 -0500

    Plot working, moveMolecule working

--- HarmonicOscillator1D.py
+++ HarmonicOscillator1D.py
@@ -6,8 +6,9 @@ import unittest
 import numpy as np
 from scipy import spatial
 import matplotlib.pyplot as plt
-from matplotlib.animation import FuncAnimation
-from scipy.optimize import fmin
+import matplotlib.lines as line
+import matplotlib.animation as animation
+from mpl_toolkits.mplot3d import Axes3D
 
 # Constants from LJ potential
 # e: H well depth
@@ -15,14 +16,19 @@ from scipy.optimize import fmin
 _e = 7.607e-19    # kJ/mol
 _d = 1.2          # angstroms
 
+# Number of atoms
+# Variable to be changed as needed
+num = 3
+
 def generateAtoms(num):
-	# Generates a random 3D array of n x 3 size
+	# Generates a random 3D array of num x 3 size
 	array = np.random.rand(num, 3)
 	array_size = array.shape
 	return array, array_size
 
 
 def euclideanDist(point1, point2):
+	# Returns euclidean distance between two points
 	return spatial.distance.euclidean(point1, point2)
 
 
@@ -35,6 +41,7 @@ def calculateV(array):
 
 	# Query tree for nearest neighbors using KD tree
 	# Returns indices of NN
+	# MAKE INTO SEPARATE FUNCTION
 	for n in points:
 		output = [i for i in points if i != n]
 		tree = spatial.KDTree(output)
@@ -47,22 +54,76 @@ def calculateV(array):
 		V.append((4 * _e) * (((_d / r) ** 12) - ((_d / r) ** 6)))
 	return V
 
-	plt.plot(points)
-	plt.show()
-
 
 def moveMolecule(array):
-	x0 = [0, 0, 0]
-	minV = fmin(calculateV,x0)
-	print minV
+	# Moves molecules around to find minimum bond distance
+	# Stop when potential below ???
+	# FIND VALUE THAT MAKES SENSE
+	# MAKE BETTER ADDARRAY
+	# Plots path of points
+	# Puts final output into Pymol
 
-	for x,y,z in array:
-		addArray = np.random.randint(low=-2, high=2, size=(len(array),3))
-		while calculateV(array) != minV:
-			array = np.add(array, addArray)
+	points = []
+	x = []
+	y = []
+	z = []
 
+	while all(V > -7.2e-19 for V in calculateV(array)):
+		addArray = np.random.uniform(low=0, high=0.001, size=(len(array),3))
+		array = np.add(array, addArray)
+		points.append(array)
+
+
+	# Extract points for plotting
+	points = np.squeeze(points)
+	print points
+	for i in points:
+		for j in i:
+			x.append(j[0])
+			y.append(j[1])
+			z.append(j[2])
+
+	finalPoints =  ???
+	putInPymol(finalPoints)
+
+	plot(x, y, z)
+
+
+def plot(x, y, z):
+	# Plots path of points
+	# Restructure so array values assigned to every N point
+	# FIX! ONLY WORKS FOR 3 PTS RN
+	ptx1 = x[0::3]
+	pty1 = y[0::3]
+	ptz1 = z[0::3]
+	ptx2 = x[1::3]
+	pty2 = y[1::3]
+	ptz2 = z[1::3]
+	ptx3 = x[2::3]
+	pty3 = y[2::3]
+	ptz3 = z[2::3]
+
+	fig = plt.figure()
+	ax = fig.add_subplot(111, projection='3d')
+
+	ax.scatter(ptx1, pty1, ptz1, c='red')
+	ax.scatter(ptx2, pty2, ptz2, c='blue')
+	ax.scatter(ptx3, pty3, ptz3, c='green')
+
+	"""
+	def animate(n, data, scatter):
+		scatter.set_array(data[n])
+   		return scatter,
+
+	ani = animation.FuncAnimation(fig, animate, frames=200)
+	"""
+
+	plt.show()
+	
 
 def putInPymol(array):
+	# Organizes array into Pymol readable format
+
 	# First cast array as strings
 	def makeStr(myArray):
 		return map(str, myArray)
@@ -73,7 +134,6 @@ def putInPymol(array):
 	flatArray = x.flatten()
 	size = flatArray.shape
 
-	# Put into PyMol readable format
 	# Must iterate every 3 as we've flattened the array
 	# Want format: ATOM      0  H0   U 0  10      x y z
 	# LATER FIX TO PUT M LOOP ON OUTSIDE--DEFINE ATOMS CORRECTLY
@@ -105,14 +165,14 @@ class Test(unittest.TestCase):
 			 [1, 1, 0],
 			 [3, 1, 0])]
 
-
+	"""
 	# Test to see if generateAtoms generates
 	# the right size array
 	def test_generateAtoms(self):
 		for [test_num, expected_size] in self.data1:
 		    (actual_array, actual_size) = generateAtoms(test_num)
 		    self.assertEqual(actual_size, expected_size)
-
+	"""
 
 	# Test to see if calculateV generates values
 	# Compare to calculated values
@@ -124,21 +184,15 @@ class Test(unittest.TestCase):
 			-1.3534136350801918e-19]
 			self.assertEqual(calculated_V, actual_V)
 
-
-
 	# Test putting arrays into Pymol coords
 	def test_putInPymol(self):
    		for array in self.easy:
    			putInPymol(array)
 
-   	
    	def test_moveMolecules(self):
    		for array in self.easy:
    			moveMolecule(array)
    	
-
-
-
 
 if __name__ == "__main__":
 	unittest.main()
