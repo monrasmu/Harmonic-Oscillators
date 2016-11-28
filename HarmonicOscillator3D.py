d1517e96d1e108ce1f4cdc137c866a269f3612be
monrasmu <monrasmu@umich.edu>
2016-11-27 21:10:25 -0500

    Plot updates

--- HarmonicOscillator3D.py
+++ HarmonicOscillator3D.py
@@ -1,15 +1,12 @@
 # Harmonic Oscillator N-D
 # Uses LJ potential to calculate minimum bond distance for n H2 atoms
-# Currently only working for 3 atoms
 
 import unittest
 import time
 import sys
-import operator
 import numpy as np
 from scipy import spatial
 import matplotlib.pyplot as plt
-import matplotlib.lines as line
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib.animation import FuncAnimation
 
@@ -22,9 +19,9 @@ _d = 1.09     # angstroms
 
 # Variable to be changed as needed
 # Number of atoms
-# Number of steps (5000 appears to be optimal)
-num = 4
-steps = 5000
+# Number of steps
+num = 10
+steps = 1000
 
 
 def doItAll(num):
@@ -34,23 +31,16 @@ def doItAll(num):
 	# Then plots points and puts final in PDB format
 	# But wait, there's more!
 	# Times the program too
-
+	newlist = []
 	tic = time.clock()
 
-	progress = 0
-        for step in range(steps):
-            progress = updateProgressBar(progress, step, steps)
-
-	print '\n'
-
 	array = generateAtoms(num)
-	arrayPoints, vArray = moveMolecule(array, numSteps=steps)
-	plotMolecule(arrayPoints)
-
-	print min(vArray), " kJ/mol is the minimum potential achieved"
-
+	arrayPoints, vArraySum = moveMolecule(array, numSteps=steps)
+	plotSetUp(arrayPoints)
+	print min(vArraySum)," kJ/mol is the minimum potential achieved for the system"
 	toc = time.clock()
-	print (toc - tic), "seconds to run full function"
+	print round((toc - tic), 3), "seconds to run"
+
 
 def updateProgressBar(progress, step, steps):
     # Progress bar
@@ -62,9 +52,10 @@ def updateProgressBar(progress, step, steps):
     sys.stdout.flush()
     return progress
 
+
 def generateAtoms(num):
 	# Generates a random 3D array of num x 3 size
-	array = np.random.rand(num, 3)
+	array = np.random.uniform(low=-0.001, high=0.001, size=(num,3))
 	array_size = array.shape
 	array = np.squeeze(array)
 	return array
@@ -75,31 +66,38 @@ def euclideanDist(point1, point2):
 	return spatial.distance.euclidean(point1, point2)
 
 
-def NNSearch(array):
-	# Query tree for nearest neighbors using KD tree
-	# Returns radius as list of NN distances
-	points = tuple(map(tuple, array))
-	radius = []
+def NNSearchSetUp(array):
+	# Takes array and assigns them to tuples
+	return tuple(map(tuple, array))
 
+
+def NNSearch(points):
+	# Query tree for nearest neighbors using KD tree
+	# Requires that points are in tuples
+	# Returns radius as list of NN distances
+	radius = []
 	for n in points:
 		output = [i for i in points if i != n]
 		tree = spatial.KDTree(output)
-		index = tree.query_ball_point(x=n, r=2)
+		index = tree.query_ball_point(x=n, r=3)
 		for i in index:
-			radius.append(euclideanDist(tree.data[i], n))
-	
+			radius.append(euclideanDist(tuple(tree.data[i]), n))
 	return radius
 
 
 def functionV(r):
-		return (4 * _e) * (((_d / r) ** 12) - ((_d / r) ** 6))
+	# Function to calculate LJ potential
+	return ((4 * _e) * (((_d / r) ** 12) - ((_d / r) ** 6)))
 
 
 def sumV(array):
 	# Uses NN search to calculate sum of potential of system
 	V = []
 
-	radius  = NNSearch(array)
+	# Performs NN search to collect nearby radii
+	points = NNSearchSetUp(array)
+
+	radius  = NNSearch(points)
 	
 	# Calculate potential for NNs
 	for r in radius:
@@ -107,7 +105,29 @@ def sumV(array):
 	return sum(V)
 
 
+def vArrays(array):
+	# Uses NN search to calculate the potential of system
+	V = []
+
+	# Performs NN search to collect nearby radii
+	points = NNSearchSetUp(array)
+
+	radius  = NNSearch(points)
+	
+	# Calculate potential for NNs
+	for r in radius:
+		V.append(functionV(r))
+	return V
+
 """
+def gradient(function):
+	radius = NNSearch(array)
+	zs = np.array([functionV(rad) for rad in radius])
+	gradx = gradient(zs)
+	print gradx
+	return np.gradient(function)
+
+
 def eulerMethod(function, array, numsteps):
 	stepSize = 0.1
 	v0 = function(array)
@@ -131,31 +151,66 @@ def eulerMethod(function, array, numsteps):
 
 def moveMolecule(array, numSteps):
 	# Moves molecules in random directions by adding random array
-	# Will not move points if sum of potential increases
+	# Will not move points if potential of system increases
 	points = []
 	vArray = []
+	vArraySum = []
+	progress = 0
 
+	points.append(array)
 	for i in range(numSteps):
-		addArray = np.random.uniform(low=-0.1, high=0.1, size=(len(array),3))
+		addArray = np.random.uniform(low=-0.1, high=0.1, size=(num,3))
 		if sumV(np.add(array, addArray)) < sumV(array):
 			array = np.add(array, addArray)
 		else: 
 			array = array
+
 		points.append(array)
-		vArray.append(sumV(array))
+		vArray.append(vArrays(array))
+		vArraySum.append(sumV(array))
 
-	return points, vArray
+		# Updates progress bar
+		progress = updateProgressBar(progress, i, numSteps)
+
+	# Pass two atoms to plotV to plot
+	plotV(points, vArray)
+
+	return points, vArraySum
 
 
-def plotMolecule(array):
+def plotV(points, vArray):
+	# Plots potential as a function of radius between two points
+	radius = []
+	vPoints = []
+	vArray = np.squeeze(vArray)
+
+	for n in range(steps):
+		radius.append(euclideanDist(points[n][0], points[n][1]))
+		vPoints.append(vArray[n][0])
+	
+	fig = plt.figure()
+	ax = fig.add_subplot(1, 1, 1)
+	ax.set_title('Potential vs Radius')
+	ax.set_xlabel('radius')
+	ax.set_ylabel('potential (kJ/mol)')
+	ax.scatter(radius, vPoints, s=50)
+	ax.set_xlim(0, 2)
+	ax.set_ylim(-0.5, 1)
+
+	x = np.linspace(0.001, 2, 1000)
+	y = functionV(x)
+	ax.plot(x, y)
+	
+
+def plotSetUp(array):
 	# Setup for plotting points
 	# Puts final output into Pymol and calls plot to plot
-
 	points = []
 	finalPointN = []
 	x = []
 	y = []
 	z = []
+	radii = []
 
 	# Extract points for plotting
 	points = np.squeeze(array)
@@ -167,20 +222,61 @@ def plotMolecule(array):
 
 	for n in range(num):
 		finalPointN.append((x[-n], y[-n], z[-n]))	
-
+	
 	putInPymol(finalPointN)
 
+	"""
+	# Prints distance to all neighbors
 	for n in range(num):
 		for m in range(1, num):
 			if n != m:
-				print 'between %i and %i: '  % ((n + 1), (m + 1)), euclideanDist(finalPointN[n], finalPointN[m])
+				# print 'between %i and %i: '  % ((n + 1), (m + 1)), euclideanDist(finalPointN[n], finalPointN[m])
+				radii.append(euclideanDist(finalPointN[n], finalPointN[m]))
+	"""
 
-	#plot(x, y, z)
+	# Prints average bond distance to neighbors
+	# print '\n', 'Average bond distance is', sum(NNSearch(finalPointN)) / len(NNSearch(finalPointN))
+
+	plot(x, y, z)
 	
 
 def plot(x, y, z):
+	# Plots initial and final output
+	ptxN = []
+	ptyN = []
+	ptzN = []
+	ptx1 = []
+	pty1 = []
+	ptz1 = []
+	for n in range(num):
+		ptxN.append(x[-n + 1])
+		ptyN.append(y[-n + 1])
+		ptzN.append(z[-n + 1])
+		ptx1.append(x[n + 1])
+		pty1.append(y[n + 1])
+		ptz1.append(z[n + 1])
+	
+	fig = plt.figure()
+	ax = fig.add_subplot(2, 1, 1, projection='3d')
+	ax.set_title('Initial')
+	ax.set_xlim3d(-1, 1)
+	ax.set_ylim3d(-1, 1)
+	ax.set_zlim3d(-1, 1)
+	ax2 = fig.add_subplot(2, 1, 2, projection='3d')
+	ax2.set_title('Final')
+	ax2.set_xlim3d(-1, 1)
+	ax2.set_ylim3d(-1, 1)
+	ax2.set_zlim3d(-1, 1)
+	colors = np.random. rand(num)
+	ax.scatter(ptx1, pty1, ptz1, c=colors, s=50)
+	ax2.scatter(ptxN, ptyN, ptzN, c=colors, s=50)
+	plt.show()
+
+
+def plotAnimation(x, y, z):
 	# Plots path of points
 	# Restructure so array values assigned to every 3 points
+	# !OJO! Currently only plotting effectively for 3 atoms
 	ptx1 = x[0::3]
 	pty1 = y[0::3]
 	ptz1 = z[0::3]
@@ -199,18 +295,18 @@ def plot(x, y, z):
 
 	# Animate, iterating by 2* i-th element from points
 	def animate(i):
-		ax.scatter(ptx1[:(2*i)], pty1[:(2*i)], ptz1[:(2*i)], s=area, c='red')
-		ax.scatter(ptx2[:(2*i)], pty2[:(2*i)], ptz2[:(2*i)], s=area, c='blue')
-		ax.scatter(ptx3[:(2*i)], pty3[:(2*i)], ptz3[:(2*i)], s=area, c='green')
+		ax.scatter(ptx1[:(5*i)], pty1[:(5*i)], ptz1[:(5*i)], s=area, c='red')
+		ax.scatter(ptx2[:(5*i)], pty2[:(5*i)], ptz2[:(5*i)], s=area, c='blue')
+		ax.scatter(ptx3[:(5*i)], pty3[:(5*i)], ptz3[:(5*i)], s=area, c='green')
 
-	ani = FuncAnimation(fig, animate, frames=len(ptx1), interval=200)
+	ani = FuncAnimation(fig, animate, frames=len(ptx1), interval=10)
 	
 	plt.show()
 	
 
 def putInPymol(array):
 	# Organizes array into Pymol readable format
-	# NEED TO FIX TO ACCEPT NEGATIVES
+	# !OJO! Need to fix to accept negative values
 
 	# Get dimensions of array and flatten to make life easier
 	x = np.array(array)
@@ -236,17 +332,6 @@ def putInPymol(array):
 		file.write('\n')
 	file.close()
 
-"""
-class Test(unittest.TestCase):
-	data = [([0, 0, 0],
-			 [1, 1, 0],
-			 [3, 1, 0])]
-
-
-   	def test_doItAll(self):
-   		doItAll(num)
-"""
 
 if __name__ == "__main__":
 	doItAll(num)
-	#unittest.main()
--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 "# Harmonic-Oscillators" 
+
 Project studying the Lennard-Jones potential of N atoms.
 
 Requires: unittest, numpy, scipy, matplotlib, mpl_toolkits.mplot3d
 
-Current status: Working for N atoms. Tends to run slow for >5 or for >5000 steps. Need to work on PutInPymol and FuncAnimation as those are not optimal right now.
+Current status: Working for N atoms. Updates progress bar to show time to run. Animation is pretty slow; found it would be simpler to plot initial and final points. Also, now plots potential between two points. Still need to fix PutInPymol
+
+Thoughts: There are definitely better ways to run such minimizations--gradient, etc. This method appears to give a successful "quick and dirty" approach--was fairly quick to code. I need to improve upon memory and time. I initialize a lot of arrays that do not need to be initialized. As well, I should work on splitting up programs into calculating and plotting. Organization could be greatly improved.
